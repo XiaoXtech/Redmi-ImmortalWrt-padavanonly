90cceb7e7edc84920a3295bedfee74ce07136500
XiaoXtech <67537423+XiaoXtech@users.noreply.github.com>
2026-02-11 06:02:27 +0800

    netspeedtest

--- immortalwrtARM/ax6000/diy1.sh
+++ immortalwrtARM/ax6000/diy1.sh
@@ -17,3 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
+git clone https://github.com/sirpdboy/luci-app-netspeedtest package/netspeedtest
